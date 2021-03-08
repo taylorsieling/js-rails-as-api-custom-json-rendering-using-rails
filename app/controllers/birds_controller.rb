3c07166162e8c07b72559e153da5125463172ada
taylorsieling <taylorasieling@gmail.com>
2021-03-08 13:45:56 -0600

    commit tree e6b9518f12fd6cd95dec3f6b9d9fe0282f894395

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,26 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render json: @birds
+    birds = Bird.all
+    render json: birds, only: [:id, :name, :species]
+
+    # you can use only: or except:
   end
+
+  def show
+    # key/hash method
+    # bird = Bird.find_by(id: params[:id])
+    # render json: {id: bird.id, name: bird.name, species: bird.species } 
+
+    # # slice method
+    # bird = Bird.find_by(id: params[:id])
+    # render json: bird.slice(:id, :name, :species)
+
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      render json: { id: bird.id, name: bird.name, species: bird.species }
+    else
+      render json: { message: 'Bird not found' }
+    end
+  end
+  
 end
